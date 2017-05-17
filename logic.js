139bb1687a7b5cff99bbf2ba508f495fe4242704
alexandrm <oleksandrm@Interlink-ua.com>
2017-05-17 21:19:34 +0300

    Reviewed and corrected

--- logic.js
+++ logic.js
@@ -1,22 +1,24 @@
-const users = [{username: 'amd', password: '123'},
-    {username: 'ama', password: '123'},
-    {username: 'ams', password: '123'}];
+const users = [
+    {username: 'user1', password: 'p@ssword'},
+    {username: 'admin', password: 'admin'},
+];
 
-document.getElementById('LogIn').onclick = function () {
-    let login = document.getElementById('name').value;
-    let pass = document.getElementById('password').value;
-    check(login, pass);
+document.forms['loginForm'].onsubmit = function () {
+    const login = document.getElementById('name').value;
+    const pass = document.getElementById('password').value;
+
+    if (isValid(login, pass)) {
+        alert('Successfully authenticated.');
+    } else {
+        alert('Incorrect username or password.\nTry again.');
+    }
 };
 
-function check(login, pass) {
-    let res;
+function isValid(login, pass) {
     for (let i = 0; i < users.length; i++) {
         if (users[i].username === login && users[i].password === pass) {
-            res = 'success';
-            break;
-        } else {
-            res = 'error'
+            return true;
         }
     }
-    alert(res);
+    return false;
 }
