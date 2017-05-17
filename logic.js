b33feff8a41969ff29fc2cb0b8e79f0e25a35ee8
alexandrm <oleksandrm@Interlink-ua.com>
2017-05-17 21:23:19 +0300

    Used array api

--- logic.js
+++ logic.js
@@ -1,24 +1,21 @@
 const users = [
-    {username: 'user1', password: 'p@ssword'},
-    {username: 'admin', password: 'admin'},
+    {login: 'user1', password: 'p@ssword'},
+    {login: 'admin', password: 'admin'},
 ];
 
-document.forms['loginForm'].onsubmit = function () {
+document.forms['loginForm'].onsubmit = function (e) {
+    e.preventDefault();
     const login = document.getElementById('name').value;
-    const pass = document.getElementById('password').value;
+    const password = document.getElementById('password').value;
 
-    if (isValid(login, pass)) {
+    if (isValid(login, password)) {
         alert('Successfully authenticated.');
     } else {
         alert('Incorrect username or password.\nTry again.');
+        this.reset();
     }
 };
 
-function isValid(login, pass) {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === login && users[i].password === pass) {
-            return true;
-        }
-    }
-    return false;
+function isValid(login, password) {
+    return users.some(user => user.login === login && user.password === password);
 }
