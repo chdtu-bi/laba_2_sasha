204e14543a1e8397ba89594aee19f59806ae5aa4
alexandrm <oleksandrm@Interlink-ua.com>
2017-05-17 21:32:26 +0300

    Refactored Added DOMContentLoaded event

--- logic.js
+++ logic.js
@@ -3,19 +3,21 @@ const users = [
     {login: 'admin', password: 'admin'},
 ];
 
-document.forms['loginForm'].onsubmit = function (e) {
-    e.preventDefault();
-    const login = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
-
-    if (isValid(login, password)) {
-        alert('Successfully authenticated.');
-    } else {
-        alert('Incorrect username or password.\nTry again.');
-        this.reset();
-    }
-};
-
 function isValid(login, password) {
     return users.some(user => user.login === login && user.password === password);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.forms['loginForm'].onsubmit = (e) => {
+        e.preventDefault();
+        const login = document.getElementById('name').value;
+        const password = document.getElementById('password').value;
+
+        if (isValid(login, password)) {
+            alert('Successfully authenticated.');
+        } else {
+            alert('Incorrect username or password.\nTry again.');
+            this.reset();
+        }
+    };
+});
